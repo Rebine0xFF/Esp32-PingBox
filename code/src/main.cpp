81769e06a931bb76b7407e72f7234155afa5ed6b
Rebine0xFF <hrobin.67000@gmail.com>
2026-05-07 19:49:54 +0200

    Add new media, hardware & design files. New code for initial testing

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
+// Définition des broches
+const int brocheLed = 2;    // GPIO 2 (souvent la LED intégrée D2)
+const int brocheBouton = 4; // GPIO 4 (D4 sur la carte) pour le bouton
+
 void setup() {
-  Serial.begin(115200);
+  // Configure la broche de la LED en sortie
+  pinMode(brocheLed, OUTPUT);
+  
+  // Configure la broche du bouton en entrée avec pull-up interne
+  pinMode(brocheBouton, INPUT_PULLUP);
 }
+
 void loop() {
-  Serial.println("hehehou");
-  delay(1000);
+  // Lecture de l'état du bouton
+  int etatBouton = digitalRead(brocheBouton);
+
+  // Si le bouton est pressé (le signal passe à LOW à cause du pull-up)
+  if (etatBouton == LOW) {
+    digitalWrite(brocheLed, HIGH); // Allume la LED
+  } else {
+    digitalWrite(brocheLed, LOW);  // Éteint la LED
+  }
 }
