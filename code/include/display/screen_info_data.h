e73b6c3190c62e0957c2aca55e8dcbe48cc2d6c3
Rebine0xFF <hrobin.67000@gmail.com>
2026-05-19 20:07:58 +0200

    refactor icon management for WiFi and server status (because of size changes) +test to cycle through statuses

--- code/include/display/screen_info_data.h
+++ code/include/display/screen_info_data.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdint.h>
 
 extern char TIME_text[];
 extern char DAY_text[];
@@ -7,7 +8,19 @@ extern char IP_ADRESS_text[];
 extern char LAST_ACTION_text[];
 
 extern const unsigned char* image_LAST_ACTION_ICON_bits;
-extern const unsigned char* image_WIFI_STATUS_bits;
-extern const unsigned char* image_SERVER_STATUS_bits;
 extern const unsigned char* image_WIFI_ICON_bits;
 extern const unsigned char* image_STATUS_FACE_bits;
+
+
+struct Icon {
+    const unsigned char* data;
+    uint8_t w;
+    uint8_t h;
+};
+enum class Status { OK, PAUSED, ERROR };
+
+extern const Icon* image_WIFI_STATUS_bits;
+extern const Icon* image_SERVER_STATUS_bits;
+
+void setWifiStatus(Status s);
+void setServerStatus(Status s);
